b2b5ae3367c9d587290625079bd17d89fc4958f6
Michael  Rondeau <themike7171@gmail.com>
2022-04-27 07:48:55 -0400

    add test

---
+++ premiere_session/P_14/AOC_2021_01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AOC_2021_01
+{
+    class Program    
+    {
+        static void Main(string[] args)
+        {
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
---
+++ premiere_session/P_14/Ex1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double averageSleep = 2598.8;
+            double lotrRuntime = 7.12;
+            int userAge;
+           
+            Console.WriteLine("Veuillez entrer votre age suivi de la touche ENTER");
+            userAge = Convert.ToInt32(Console.ReadLine());
+
+            if(userAge < 18) {
+                Console.WriteLine("Désolé, il n'y a pas de données pour les 18 ans et moins.");
+            }
+            else {
+            int adultYears = userAge -18;
+            double hoursSlept = adultYears * averageSleep;
+            double missedOpportunity = hoursSlept / lotrRuntime;
+
+            Console.WriteLine($"Vous avez dormis un total de {hoursSlept} heures.  Vous auriez pu écouter la trilogie version longue du seigneur des anneaux {missedOpportunity} fois. Dommage... ");
+
+            }
+
+        }
+    }
+}
