a72997a774adbfbe7d49e47b596ddbfe26177481
Michael  Rondeau <themike7171@gmail.com>
2022-04-28 16:01:20 -0400

    fix indentation

--- premiere_session/P_14/Ex2/Program.cs
+++ premiere_session/P_14/Ex2/Program.cs
@@ -6,68 +6,67 @@ namespace Ex2
     {
         static void Main(string[] args)
         {
-           //Declarer et initialiser constantes
-string MSG_ANALYSTE = "Sans nous il n'y a pas de structure!";
-string MSG_PROG = "C'est nous qui faisons tout!";
-string MSG_ADMIN_RESEAU = "Sans nous tout ça ne sert à rien!";
+                //Declarer et initialiser constantes
+        string MSG_ANALYSTE = "Sans nous il n'y a pas de structure!";
+        string MSG_PROG = "C'est nous qui faisons tout!";
+        string MSG_ADMIN_RESEAU = "Sans nous tout ça ne sert à rien!";
 
-//Declarer variables
-string	msgInstructions;
-char	titrePoste = default;
-char	reponse;
-int	nbrAnalystes = 0 ;
-int	nbrProgs = 0 ;
-int	nbrAdminsReseau = 0 ;
-bool	continuer;
-bool valide = false ;
+        //Declarer variables
+        string	msgInstructions;
+        char	titrePoste = default;
+        char	reponse;
+        int	nbrAnalystes = 0 ;
+        int	nbrProgs = 0 ;
+        int	nbrAdminsReseau = 0 ;
+        bool	continuer;
+        bool valide = false ;
 
-// Assignations prealables
-msgInstructions = "Veuillez entrer le caractere correspondant au poste. (A)nalyste, (P)rogrammeur et (R)eseau.";
+        // Assignations prealables
+        msgInstructions = "Veuillez entrer le caractere correspondant au poste. (A)nalyste, (P)rogrammeur et (R)eseau.";
 
-continuer = true;
+        continuer = true;
 
-while(continuer) {
-	//Afficher instructions et faire la saisie
-    while(valide == false){
-    try{
-	Console.WriteLine(msgInstructions);
-        titrePoste =Char.Parse((Console.ReadLine().ToUpper()));
-	if(titrePoste == 'A'){
-		Console.WriteLine(MSG_ANALYSTE);
-		nbrAnalystes ++;
-        valide = true;
-    }else if(titrePoste == 'P'){
-		Console.WriteLine(MSG_PROG);
-		nbrProgs++;
-        valide = true;
-    }else if(titrePoste == 'R')
-		{Console.WriteLine(MSG_ADMIN_RESEAU);
-		nbrAdminsReseau ++;
-        valide = true;
+        while(continuer) {
+            //Afficher instructions et faire la saisie
+            while(valide == false){
+                try{
+                    Console.WriteLine(msgInstructions);
+                    titrePoste =Char.Parse((Console.ReadLine().ToUpper()));
+                    if(titrePoste == 'A'){
+                        Console.WriteLine(MSG_ANALYSTE);
+                        nbrAnalystes ++;
+                        valide = true;
+                    }else if(titrePoste == 'P'){
+                        Console.WriteLine(MSG_PROG);
+                        nbrProgs++;
+                        valide = true;
+                    }else if(titrePoste == 'R')
+                        {Console.WriteLine(MSG_ADMIN_RESEAU);
+                        nbrAdminsReseau ++;
+                        valide = true;
+                        }
+            }catch(Exception e){
+                Console.WriteLine("ERREUR ERREUR ALERTE ALERT MAUDIT NONO");
+
+            }
+            }
+            valide = false;
+            Console.WriteLine("Voulez-vous entrez le poste d'une autre personne? (O)ui pour continuer.");
+            //continuer <-- Saisir() == 'O'
+            reponse = Char.Parse(Console.ReadLine().ToUpper());
+            continuer = reponse == 'O';
         }
-    }catch(Exception e){
-        Console.WriteLine("ERREUR ERREUR ALERTE ALERT MAUDIT NONO");
+        Console.WriteLine("Voulez-vous afficher les statistiques?. (O)ui pour afficher (N)on pour quitter.");
+        reponse = Char.Parse(Console.ReadLine().ToUpper());
 
-    }
-    }
-    valide = false;
-	Console.WriteLine("Voulez-vous entrez le poste d'une autre personne? (O)ui pour continuer.");
-	//continuer <-- Saisir() == 'O'
-	reponse = Char.Parse(Console.ReadLine().ToUpper());
-	continuer = reponse == 'O';
-}
-Console.WriteLine("Voulez-vous afficher les statistiques?. (O)ui pour afficher (N)on pour quitter.");
-reponse = Char.Parse(Console.ReadLine().ToUpper());
-
-if(reponse == 'O'){
-	Console.WriteLine($"nombre analystes       : {nbrAnalystes}");
-	Console.WriteLine($"nombre programmeurs    : {nbrProgs}");
-	Console.WriteLine($"nombre d'admins réseau : {nbrAdminsReseau}");
-}
-else if(reponse == 'N')
-	{Console.WriteLine("Mais pourquoi?");
-
-    }
+        if(reponse == 'O'){
+            Console.WriteLine($"nombre analystes       : {nbrAnalystes}");
+            Console.WriteLine($"nombre programmeurs    : {nbrProgs}");
+            Console.WriteLine($"nombre d'admins réseau : {nbrAdminsReseau}");
+        }
+        else if(reponse == 'N')
+            {Console.WriteLine("Mais pourquoi?");
+            }
 
         }
     }
