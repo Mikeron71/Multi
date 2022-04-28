1ad51ed325aab1598641f5380469b4f779eb64cb
Michael  Rondeau <themike7171@gmail.com>
2022-04-28 15:54:05 -0400

    ex2

--- premiere_session/P_14/AOC_2021_01/Program.cs
+++ premiere_session/P_14/AOC_2021_01/Program.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 
+
 namespace AOC_2021_01
 {
     class Program    
     {
         static void Main(string[] args)
         {
-        
+            
     
 
+            int[] chiffres = new int[] {119,121,129,128,130,134};
+            
+         
 
-
+            for (int i = 0; i < chiffres.Length; i++)
+            {
+                
+            Console.WriteLine(chiffres[i]);
+            }
 
 
 
