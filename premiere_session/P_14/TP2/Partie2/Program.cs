dea01436129b27ff7d28789dbb0cb47a60a6cf54
Michael  Rondeau <themike7171@gmail.com>
2022-05-16 07:17:07 -0400

    test

--- premiere_session/P_14/TP2/Partie2/Program.cs
+++ premiere_session/P_14/TP2/Partie2/Program.cs
@@ -21,6 +21,7 @@ namespace partie2
             double mois = 0;
             decimal interetGagne;
             decimal montant = 0;
+            char reponse;
             
             
 
@@ -35,7 +36,7 @@ namespace partie2
                 }while(!valide);
                 do{
                     Console.WriteLine("Veuillez entrer le taux d'intérêts annuel. Entrez 10 pour 10%");
-                    valide = double.TryParse  (Console.ReadLine(),out tauxAnnuel);
+                    valide = double.TryParse  (Console.ReadLine(),out tauxAnnuel) && tauxAnnuel > 0;
                 
 
 
@@ -49,101 +50,40 @@ namespace partie2
 
                 //Calculs 
 
-                
-                  do{  
-                    
-                  
-                    // interetGagne = (decimal)Math.Pow(depotInitial*(1+tauxAnnuel/12/100),12);
-
-
-                double tauxmensuel = tauxAnnuel/12;
-
-                montant = depotInitial*(decimal)Math.Pow((1+tauxmensuel/100),1) + depotsSubsequents;
-                interetGagne = montant-depotInitial;
-                depotInitial += interetGagne ;
-                mois++;
-
             
-                if(mois % 12 ==0){
+                do{ 
+                    double tauxmensuel = tauxAnnuel/12;
+                    montant = depotInitial*(decimal)Math.Pow((1+tauxmensuel/100),1) + depotsSubsequents;
+                    interetGagne = montant-depotInitial;
+                    depotInitial += interetGagne ;
+                    mois++;
 
-                var table = new ConsoleTable("Catégorie","","");
+                    // je fais modelo pour afficher seulement les annees
+                    if(mois % 12 ==0){
+
+                        var table = new ConsoleTable("Catégorie","","");
                                 
                         
-                string fleche = "------------------------>";
-                table.AddRow($"année {mois/12}",fleche ,  montant.ToString("c",myCIintl)    );
-                Console.WriteLine(table);
-                }
-
-
-               
-
-            
-                  }
-                while (montant <=1000000);
+                        string fleche = "------------------------>";
+                        table.AddRow($"année {mois/12}",fleche ,  montant.ToString("c",myCIintl)    );
+                        Console.WriteLine(table);
+                    }
+                } while (montant <=1000000);
               
-              int annes = (int)Math.Floor(mois/12);
-              int moisRestants = (int)mois %12;
+                int annes = (int)Math.Floor(mois/12);
+                int moisRestants = (int)mois %12;
 
                 Console.WriteLine($"En {annes} ans et {moisRestants} mois vous aurrez atteint le million de dollars. Plus précisément {montant.ToString("c",myCIintl)}.");
 
+                do{
+                    Console.WriteLine("Voulez vous calculer une autre hypoteque? (O)ui ou (N)on");
+                    valide = Char.TryParse(Console.ReadLine().ToUpper(), out reponse) && reponse == 'O' || reponse =='N';}
+                    while(!valide);
+
+                continuer = (reponse =='O')?  true : false;
                    
-                   
-               
-
-                
-                
-
-
-
-
-
-
-
-
-                              
-            //     //Config tableau resultats
-            //     var table = new ConsoleTable("Catégorie","","");
-                                
-                        
-            //     string fleche = "------------------------>";
-            //     table.AddRow("Nombre de versements",fleche        ,versementsUnAn*amortissement)
-            //     .AddRow("Paiement de capital",fleche              ,montantPret.ToString("c", myCIintl))
-            //     .AddRow("Paiement de frais d'interêts", fleche    ,interets.ToString("c", myCIintl) )
-            //     .AddRow("Coût total", fleche                      ,total.ToString("c", myCIintl) );
-            //     Console.WriteLine(table);
-                                
-            //     do{
-            //         Console.WriteLine("Voulez vous calculer une autre hypoteque? (O)ui ou (N)on");
-            //         valide = Char.TryParse(Console.ReadLine().ToUpper(), out reponse) && reponse == 'O' || reponse =='N';}
-            //         while(!valide);
-
-            //     continuer = (reponse =='O')?  true : false;
-
                 
             } while (continuer == true);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
     }
 }
