6cf5c352f4d04d33d692def3a2fbc2d5a020d953
Michael  Rondeau <themike7171@gmail.com>
2022-06-14 10:57:32 -0400

    debut tp1 P24

---
+++ session2/P24/TP1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace TP1
+{
+    class Program
+    {   static int nbTickets;
+        static Random hasard = new Random(DateTime.Now.Millisecond);
+        static void Main(string[] args)
+        {   bool continuer = false;
+            do{
+                askUser();
+                generateCombination();
+                int[][] tickets = generateTickets();
+                int [] winnigCombination = generateCombination();
+                int complementaire = hasard.Next(0,50);
+                // generateStats();
+                // showStats();
+
+
+            }while(continuer);
+            
+        }
+        static void askUser(){
+            bool valid = false;
+            do{
+                Console.WriteLine("Hi!  How many tickets do you need?");
+                valid = Int32.TryParse(Console.ReadLine(),out nbTickets) && nbTickets > 9 && nbTickets < 201;
+            }while(!valid);
+        }
+        
+        static int[] generateCombination(){
+            int[] lign = new int[6];
+            for (int i = 0; i < 6; i++)
+            {  
+                lign[i] = hasard.Next(0,50);
+            }
+
+            
+                return lign;
+        }
+        
+        
+        static int[][] generateTickets(){
+            
+             int[][] jaggedArray = new int[nbTickets][];
+           
+
+           for (int i = 0; i < nbTickets; i++){
+                jaggedArray[i] = generateCombination();
+           }
+
+            for (int i = 0; i < jaggedArray.Length;i++){
+                Console.WriteLine();
+                Console.Write($"ticket{i+1} :    ");
+                for (int j = 0; j < 6; j++){ 
+                    Console.Write(jaggedArray[i][j]);
+                    Console.Write(" ");
+                }
+            }
+            return jaggedArray;
+        }
+    }
+}
