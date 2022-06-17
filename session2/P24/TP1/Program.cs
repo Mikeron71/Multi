2fba72bd9e4c2c4638fd7544988760c2753fe527
mike tab <themikr7171@gmail.com>
2022-06-16 21:04:00 -0400

    progres validation billet

--- session2/P24/TP1/Program.cs
+++ session2/P24/TP1/Program.cs
@@ -12,8 +12,16 @@ namespace TP1
             do{
                 askUser();
                 generateCombination(6);
-                generateTickets();
-                int [] winningCombination = generateCombination(7);
+                tickets = generateTickets();
+                winningCombination = generateCombination(7);
+                Console.Write(winningCombination[0] +" ");
+                Console.Write(winningCombination[1] +" ");
+                Console.Write(winningCombination[2] +" ");
+                Console.Write(winningCombination[3]+" ");
+                Console.Write(winningCombination[4]+" ");
+                Console.Write(winningCombination[5]+" ");
+                Console.Write(winningCombination[6]+" ");
+
                 validateTickets();
 
                 // generateStats();
@@ -28,7 +36,7 @@ namespace TP1
             bool valid = false;
             do{
                 Console.WriteLine("Hi!  How many tickets do you need?");
-                valid = Int32.TryParse(Console.ReadLine(),out nbTickets) && nbTickets > 9 && nbTickets < 201;
+                valid = Int32.TryParse(Console.ReadLine(),out nbTickets) && nbTickets > 9 && nbTickets < 999999;
             }while(!valid);
         }
         
@@ -51,14 +59,15 @@ namespace TP1
         }
         
         
-        static void generateTickets(){
+        static int[][] generateTickets(){
             
-             int[][] tickets = new int[nbTickets][];
+              int[][] tickets = new int[nbTickets][];
            
 
            for (int i = 0; i < nbTickets; i++){
                 tickets[i] = generateCombination(6);
            }
+
 
             for (int i = 0; i < tickets.Length;i++){
                 Console.WriteLine();
@@ -69,10 +78,79 @@ namespace TP1
                 }
             }
           Console.WriteLine();
+          return tickets;
         }
         static void validateTickets(){
-         
-            Console.Write(tickets[0][0]);
+         int countnum1 = 0;
+         int countnum2 = 0;
+         int countnum3 = 0;
+         int countnum4 = 0;
+         int countnum5 = 0;
+         int countnum6 = 0;
+         int countcomp = 0;
+
+         int num1 = winningCombination[0];
+         int num2 = winningCombination[1];
+         int num3 = winningCombination[2];
+         int num4 = winningCombination[3];
+         int num5 = winningCombination[4];
+         int num6 = winningCombination[5];
+         int comp = winningCombination[6];
+
+         Console.WriteLine("num1 =="+ num1);
+
+        for (int i = 0; i < tickets.Length;i++){
+            bool containsComp = false;
+            int countWin = 0;
+                
+            if( Array.Exists(tickets[i], element => element == num1)) {
+                countnum1++;
+                countWin++;
+            }
+            if( Array.Exists(tickets[i], element => element == num2)) {
+                    countnum2++;
+                    countWin++;
+            }if( Array.Exists(tickets[i], element => element == num3)) {
+                    countnum3++;
+                    countWin++;
+            }if( Array.Exists(tickets[i], element => element == num4)) {
+                    countnum4++;
+                    countWin++;
+            }if( Array.Exists(tickets[i], element => element == num5)) {
+                    countnum5++;
+                    countWin++;
+            }if( Array.Exists(tickets[i], element => element == num6)) {
+                    countnum6++;
+                    countWin++;
+            }if( Array.Exists(tickets[i], element => element == comp)) {
+                    countcomp++;
+                    containsComp = true;
+            }
+
+            if(countWin ==3) {
+                Console.WriteLine($" ticket {i+1} est un billet gagnant de 3");
+                
+            }
+             if(countWin ==4) {
+                Console.WriteLine($" ticket {i+1} est un billet gagnant de 4");
+                
+            } if(countWin ==5) {
+                Console.WriteLine($" ticket {i+1} est un billet gagnant de 5");
+                
+            } if(countWin ==6) {
+                Console.WriteLine($" ticket {i+1} est un billet gagnant de 6");
+                
+            }
+
+        }
+        Console.Write("countcount ===" + countnum1);
+        Console.Write("countcount ===" + countnum2);
+        Console.Write("countcount ===" + countnum3);
+        Console.Write("countcount ===" + countnum4);
+        Console.Write("countcount ===" + countnum5);
+        Console.Write("countcount ===" + countnum6);
+        Console.Write("countcount ===" + countcomp);
+                
 
         }
     }
