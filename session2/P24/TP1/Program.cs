c5edc1a735d63048dead0cc06c06f7b4806f463e
Michael  Rondeau <themike7171@gmail.com>
2022-06-21 19:46:55 -0400

    tab

--- session2/P24/TP1/Program.cs
+++ session2/P24/TP1/Program.cs
@@ -15,15 +15,9 @@ namespace TP1
             
                 tickets = generateTickets();
                 winningCombination = generateCombination(7);
-                // Array.Sort(winningCombination);
-                Console.Write(winningCombination[0] +" ");
-                Console.Write(winningCombination[1] +" ");
-                Console.Write(winningCombination[2] +" ");
-                Console.Write(winningCombination[3]+" ");
-                Console.Write(winningCombination[4]+" ");
-                Console.Write(winningCombination[5]+" ");
-                Console.Write(winningCombination[6]+" ");
+                afficherWinning();
 
+            
                 validateTickets();
 
                
@@ -69,16 +63,16 @@ namespace TP1
 
            for (int i = 0; i < nbTickets; i++){
                 tickets[i] = generateCombination(6);
-            
-                
            }
 
 
             for (int i = 0; i < tickets.Length;i++){
+                if(i % 5 == 0 ){
                 Console.WriteLine();
-                Console.Write( $"ticket {i+1} :    ");
+                }
+                Console.Write( $"         ticket {i+1} :".PadLeft(3));
                 for (int j = 0; j < 6; j++){ 
-                    Console.Write(tickets[i][j] + " ");
+                    Console.Write(tickets[i][j].ToString().PadLeft(3) +  " ");
                     
                 }
             }
@@ -87,12 +81,12 @@ namespace TP1
         }
         static void validateTickets(){
             int countNum1 = 0;
-            int countnum2 = 0;
-            int countnum3 = 0;
-            int countnum4 = 0;
-            int countnum5 = 0;
-            int countnum6 = 0;
-            int countcomp = 0;
+            int countNum2 = 0;
+            int countNum3 = 0;
+            int countNum4 = 0;
+            int countNum5 = 0;
+            int countNum6 = 0;
+            int countComp = 0;
 
             int num1 = winningCombination[0];
             int num2 = winningCombination[1];
@@ -102,7 +96,7 @@ namespace TP1
             int num6 = winningCombination[5];
             int comp = winningCombination[6];
 
-            Console.WriteLine("num1 =="+ num1);
+   
 
             for (int i = 0; i < tickets.Length;i++){
                 bool containsComp = false;
@@ -113,55 +107,74 @@ namespace TP1
                     countWin++;
                 }
                 if( Array.Exists(tickets[i], element => element == num2)) {
-                        countnum2++;
+                        countNum2++;
                         countWin++;
                 }if( Array.Exists(tickets[i], element => element == num3)) {
-                        countnum3++;
+                        countNum3++;
                         countWin++;
                 }if( Array.Exists(tickets[i], element => element == num4)) {
-                        countnum4++;
+                        countNum4++;
                         countWin++;
                 }if( Array.Exists(tickets[i], element => element == num5)) {
-                        countnum5++;
+                        countNum5++;
                         countWin++;
                 }if( Array.Exists(tickets[i], element => element == num6)) {
-                        countnum6++;
+                        countNum6++;
                         countWin++;
                 }if( Array.Exists(tickets[i], element => element == comp)) {
-                        countcomp++;
+                        countComp++;
                         containsComp = true;
                 }
 
                 if(countWin ==2 && containsComp == false ) {
-                Console.WriteLine($" ticket {i+1} remporte une participation gratuite");
+                Console.WriteLine($"ticket {i+1} remporte une participation gratuite");
                 }
                 
                 if(countWin ==2 && containsComp == true ) {
-                Console.WriteLine($" ticket {i+1} remporte 5$ ");
+                Console.WriteLine($"ticket {i+1} remporte 5$ ");
                 }
 
                 if(countWin ==3) {
-                    Console.WriteLine($" ticket {i+1} est un billet gagnant de 10$");
+                    Console.WriteLine($"ticket {i+1} est un billet gagnant de 10$");
                 }
                 if(countWin ==4) {
-                Console.WriteLine($" ticket {i+1} est un billet gagnant de 4");
+                Console.WriteLine($"ticket {i+1} est un billet gagnant de 4");
                 }
                     
                 if(countWin ==5) {
-                    Console.WriteLine($" ticket {i+1} est un billet gagnant de 5");
+                    Console.WriteLine($"ticket {i+1} est un billet gagnant de 5");
 
                 } if(countWin ==5 && containsComp == true) {
-                Console.WriteLine($" ticket {i+1} est un billet gagnant de 5 + C");
+                Console.WriteLine($"ticket {i+1} est un billet gagnant de 5 + C");
                 
                     
                 } if(countWin ==6) {
-                    Console.WriteLine($" ticket {i+1} est un billet gagnant de 6");
+                    Console.WriteLine($"ticket {i+1} est un billet gagnant de 6");
                     
                 }
 
              }
    
-         Console.WriteLine($"Le chiffre{winningCombination[0]} est sorti {countNum1} fois" );
+          Console.WriteLine($"Le chiffre {winningCombination[0]} est sorti {countNum1} fois" );
+          Console.WriteLine($"Le chiffre {winningCombination[1]} est sorti {countNum2} fois" );
+          Console.WriteLine($"Le chiffre {winningCombination[2]} est sorti {countNum3} fois" );
+          Console.WriteLine($"Le chiffre {winningCombination[3]} est sorti {countNum4} fois" );
+          Console.WriteLine($"Le chiffre {winningCombination[4]} est sorti {countNum5} fois" );
+          Console.WriteLine($"Le chiffre {winningCombination[5]} est sorti {countNum6} fois" );
+          Console.WriteLine($"Le chiffre {winningCombination[6]} est sorti {countComp} fois" );
          }
-}
+         static void afficherWinning(){
+            Console.WriteLine("Voici la combinaison gagnante : " );
+            for (int i = 0; i < winningCombination.Length;  i++)
+              {
+                if(i == 6){
+                    Console.WriteLine("Complémentaire: " + winningCombination[i]);
+                } else{
+
+                Console.Write(winningCombination[i] + " ");
+                }
+                
+            }
+         }
+    }
 }
---
+++ session2/P34/labFonction/Labfonction/Labfonction/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        PremiereFunc()
+        PremierSub(15)
+        PremierSub("youpi")
+        DeuxiemeSub("yes", 1)
+        MegaSub()
+
+    End Sub
+    Private Sub PremierSub()
+
+    End Sub
+    Private Sub PremierSub(premierInt As Integer)
+
+    End Sub
+    Private Sub PremierSub(premierString As String)
+
+    End Sub
+
+    Private Sub DeuxiemeSub(str As String, chiffre As Integer)
+
+    End Sub
+
+
+    Private Function PremiereFunc() As String
+        Dim j As String = "coucou"
+        Return j
+    End Function
+
+    Private Sub MegaSub(Optional param1 As String = "wow")
+    End Sub
+
+    Private Function PremiereFunc(superString As String, Optional estActif As Boolean = False)
+        Dim j As String = "coucou"
+        Return j
+    End Function
+
+End Class
